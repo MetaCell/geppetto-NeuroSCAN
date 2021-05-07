da3374583cd5b11de92ea455a79d0e086fe5797b
Lucas Rebscher <luc.rebscher@gmail.com>
2021-05-07 13:44:34 +0200

    Add backend, frontend and deployment structure
    Refs #13 #16 #15

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '43e8e5384c447752459151d927093cec'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '783fdda3-5daf-4883-9fda-49756179fd10'
+};
