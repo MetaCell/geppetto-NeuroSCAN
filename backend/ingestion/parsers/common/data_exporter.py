1bd2e61ca1748a610a8c7c4fed8fe3ee2697aa29
afonsobspinto <afonsobspinto@gmail.com>
2021-06-04 19:53:23 +0100

    #78 Add data to csv

--- backend/ingestion/parsers/common/data_exporter.py
+++ backend/ingestion/parsers/common/data_exporter.py
@@ -13,7 +13,7 @@ class DataExporter:
       writer = csv.DictWriter(csvfile, fieldnames=headers)
       writer.writeheader()
       for data in dict_data:
-        writer.writerow(data)
+        writer.writerow(dict_data[data])
 
   def simplify_data(self, headers):
     simplified_data = {}
@@ -26,11 +26,15 @@ class DataExporter:
           simplified_data[key] = self._merge_data(simplified_data[key], entry_data, headers)
     return simplified_data
 
+
   def _merge_data(self, source1, source2, headers):
     merged_data = {}
     for h in headers:
       if source1[h] and source2[h]:
-        merged_data[h] = [source1[h], source2[h]]
+        if source1[h] == source2[h]:
+          merged_data[h] = source1[h]
+        else:
+          merged_data[h] = [source1[h], source2[h]]
         continue
       if source1[h]:
         merged_data[h] = source1[h]
@@ -41,13 +45,11 @@ class DataExporter:
       merged_data[h] = None
     return merged_data
 
-
   def _get_header_attributes(self, source, headers):
     entry_data = {}
     for h_attr in headers:
       entry_data[h_attr] = source[h_attr] if h_attr in source.keys() else None
     return entry_data
-
 
   def extrapolate_headers(self):
     headers = set()
