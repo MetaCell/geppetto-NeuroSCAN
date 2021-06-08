911f00daf0b686cf6e6fb5c39d9fbc0a9dad39ab
afonsobspinto <afonsobspinto@gmail.com>
2021-06-08 15:37:24 +0100

    #78 Add unpaired_mtl_files script

---
+++ ingestion/scripts/get_list_of_unpaired_mtl_files.py
@@ -0,0 +1,21 @@
+import os
+
+
+def parse(directory, valid_extensions):
+    for filename in os.listdir(directory):
+        if filename_check(filename, valid_extensions):
+            base = os.path.splitext(filename)[0]
+            if not os.path.isfile(os.path.join(directory, base+'.obj')):
+                print(filename)
+            else:
+                continue
+
+
+def filename_check(filename, valid_extensions=None):
+    if valid_extensions is None:  # No extension means we will look into all the files
+        return True
+    return any(filename.endswith(e) for e in valid_extensions)
+
+
+if __name__ == '__main__':
+    parse('../data/neurons', ['.mtl'])
