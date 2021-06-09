f5812581819eba90a50dfeadfd956ef571407df6
afonsobspinto <afonsobspinto@gmail.com>
2021-06-09 12:13:29 +0100

    #78 Update cphate json generator

--- ingestion/parsers/c-phate/generate_json.py
+++ ingestion/parsers/c-phate/generate_json.py
@@ -1,5 +1,10 @@
 import json
+import re
+
 import pandas as pd
+
+from ingestion.parsers.common.filename_parser import FilenameParser
+from ingestion.parsers.common.utils import Config
 
 NEURON_COL_NAME = 'Neuron'
 ITER_COL_IDENTIFIER = 'iter'
@@ -11,13 +16,19 @@ def get_groups_from_xlsx(filepath):
     for row in df[coi].to_numpy():
         neuron = row[0]
         for i, g in enumerate(row[1:]):
-            group = f'g{g}'
-            iter = get_iter_key(i+1)
+            group = get_group_key(g)
+            iter = get_iter_key(i + 1)
             if group in group_dict[iter]:
-                group_dict[iter][group].append(neuron)
+                group_dict[iter][group]['neurons'].append(neuron)
             else:
-                group_dict[iter][group] = [neuron]
+                group_dict[iter][group] = {'neurons': [neuron]}
     return group_dict
+
+
+def get_filename_data():
+    obj_parser = FilenameParser(Config('filename_config.json').get_config())
+    obj_parser.parse()
+    return obj_parser.get_data()
 
 
 def get_iter_from_header(df):
@@ -44,14 +55,47 @@ def get_iter_key(i):
     return f'i{i}'
 
 
+def get_group_key(g):
+    return f'g{g}'
+
+
+def add_objfile(data_dict):
+    filename_data = get_filename_data()
+    for i in filename_data:
+        split_i = i.split('-')
+        iter = get_iter_key(split_i[0])
+        group = get_group_key(split_i[1])
+        for source in filename_data[i]:
+            data_dict[iter][group]['objFile'] = filename_data[i][source]['files']
+    return data_dict
+
+
+def dict_to_array(data_dict):
+    data_array = []
+    for i in data_dict:
+        for g in data_dict[i]:
+            data_array.append(
+                {
+                    'i': get_number(i),
+                    'g': get_number(g),
+                    **data_dict[i][g]
+                }
+            )
+    return data_array
+
+
+def get_number(string):
+    return int(re.findall(r'\d+', string)[0])
+
+
 def write_json(data, output_file):
     with open(output_file, 'w') as outfile:
         json.dump(data, outfile)
 
 
 def generate_json(filepath, output_file='output.json'):
-    write_json(get_groups_from_xlsx(filepath), output_file)
+    write_json(dict_to_array(add_objfile(get_groups_from_xlsx(filepath))), output_file)
 
 
 if __name__ == '__main__':
-    generate_json('../../data/JSH_CPHATE clusters.xlsx', 'cphate_matrix.json')
+    generate_json('../../data/JSH_CPHATE clusters.xlsx', 'cphate.json')
