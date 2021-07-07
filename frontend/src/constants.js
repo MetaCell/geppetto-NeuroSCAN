ff009a09d49733d9e39e5ef299be42068a1cc73d
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-07-07 19:52:00 +0530

    #92 Links to different dashboards from menu

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 const VIEWS = {
-  promoterDB: { route: '/promoter', title: 'Promoter DB', linkTo: 'NeuroSCAN' },
-  neuroScan: { route: '/', title: 'NeuroSCAN', linkTo: 'Promoter DB' },
+  promoterDB: { title: 'Promoter DB', linkTo: 'NeuroSCAN', linkToRoute: '/' },
+  neuroScan: { title: 'NeuroSCAN', linkTo: 'Promoter DB', linkToRoute: '/promoter' },
 };
 
 export default VIEWS;
