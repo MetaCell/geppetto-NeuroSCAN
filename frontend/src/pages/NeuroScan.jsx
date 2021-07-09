0b70c5fa95a24a118cec2b42093d722eeddd2e68
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-07-09 16:52:09 +0530

    Layout fixes, sidebar toggle, remove right panel

--- frontend/src/pages/NeuroScan.jsx
+++ frontend/src/pages/NeuroScan.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { Box, makeStyles, Typography } from '@material-ui/core';
 import LeftSidebar from '../components/LeftSidebar';
 import Header from '../components/Header';
-import RightSidebar from '../components/RightSidebar';
 import VIEWS from '../constants';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.down('xs')]: {
+      '& .primary-structure': {
+        paddingTop: '5.625rem',
+      },
+    },
+  },
+}));
+
 export default function NeuroScan() {
+  const classes = useStyles();
+
+  const [toggle, setToggle] = React.useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <>
-      <Header view={VIEWS.neuroScan} />
-      <Box display="flex">
-        <LeftSidebar />
-        <Box className="MuiBox-content">
-          <Box className="MuiBox-empty">
+    <Box className={classes.root}>
+      <Header toggle={toggle} toggleSidebar={handleToggle} view={VIEWS.neuroScan} />
+      <Box className="primary-structure" display="flex">
+        <LeftSidebar toggle={toggle} />
+        <Box className="primary-structure_content">
+          <Box className="MuiBox-empty" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
             <Typography variant="h2">
               No Elements Added yet.
             </Typography>
@@ -22,8 +38,7 @@ export default function NeuroScan() {
             </Typography>
           </Box>
         </Box>
-        <RightSidebar />
       </Box>
-    </>
+    </Box>
   );
 }
