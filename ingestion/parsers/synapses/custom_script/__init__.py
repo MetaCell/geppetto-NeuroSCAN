c2146a2a741c5115051bb87a8ec66728c20a9ed0
afonsobspinto <afonsobspinto@gmail.com>
2021-08-03 20:35:02 +0100

    #78 Convert neuronSite index to value

--- ingestion/parsers/synapses/custom_script/__init__.py
+++ ingestion/parsers/synapses/custom_script/__init__.py
@@ -1,8 +1,10 @@
 def custom_script(data):
     for tp in data:
         for synapse in data[tp]:
-            source = list(data[tp][synapse].keys())[0]
+            source = list(data[tp][synapse].keys())[0]  # synapses only have 1 source
             data[tp][synapse] = {source: data[tp][synapse][source]}
             data[tp][synapse][source]['neuronPost'] = data[tp][synapse][source]['neuronPost'].split('&')
             data[tp][synapse][source]['position'] = data[tp][synapse][source]['position'].lower()
+            data[tp][synapse][source]['neuronSite'] = data[tp][synapse][source]['neuronPost'][
+                int(data[tp][synapse][source]['neuronSite'])]
     return data
