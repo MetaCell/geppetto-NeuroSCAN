4e0571c69e88fa27ef2cdcdf6d94b3d3feba289e
afonsobspinto <afonsobspinto@gmail.com>
2021-08-03 18:47:46 +0100

    #78 Update neuron custom script

--- ingestion/parsers/neurons/custom_script/__init__.py
+++ ingestion/parsers/neurons/custom_script/__init__.py
@@ -1,5 +1,15 @@
+import json
+
+
 def custom_script(data):
-    for neuron in data:
-        if 'DCR_PromoterPipeline_Revised2021_Updated.xlsx' in data[neuron].keys():
-            data[neuron]['DCR_PromoterPipeline_Revised2021_Updated.xlsx']['embrionic'] = True
+    for tp in data:
+        for neuron in data[tp]:
+            if '../../data/promoterdb/DCR_PromoterPipeline_Revised2021_Updated.xlsx_Cell-ID based on lineaging' in \
+                    data[tp][neuron].keys():
+                data[tp][neuron][
+                    '../../data/promoterdb/DCR_PromoterPipeline_Revised2021_Updated.xlsx_Cell-ID based on lineaging'][
+                    'embrionic'] = True
+            for source in data[tp][neuron]:
+                if 'timepoints' in data[tp][neuron][source]:
+                    data[tp][neuron][source]['timepoints'] = json.dumps([data[tp][neuron][source]['timepoints']])
     return data
