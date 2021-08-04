26e4daa0c7cd2481f8132d13c2e4482b264957c4
afonsobspinto <afonsobspinto@gmail.com>
2021-08-04 10:24:31 +0100

    #77 Increase form max file size

--- backend/config/middleware.js
+++ backend/config/middleware.js
@@ -1,5 +1,14 @@
 module.exports = {
   settings: {
+    parser: {
+      formLimit: '50mb',
+      jsonLimit: '50mb',
+      enabled: true,
+      multipart: true,
+      formidable: {
+        maxFileSize: 10737418240
+      }
+    },
     cors: {
       origin: ['http://localhost:8000'],
     },
