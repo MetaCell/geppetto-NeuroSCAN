e3a65910bc62f0fa057e1ff8a771d43928d48889
vidhya-metacell <vidhya@metacell.us>
2021-08-31 14:25:40 +0530

    #149 Search bar input chips feature

--- frontend/src/components/Sidebar/Search.jsx
+++ frontend/src/components/Sidebar/Search.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
-  TextField, Box, Typography, InputAdornment, IconButton,
+  Box, Typography, IconButton, Chip,
 } from '@material-ui/core';
+import ChipInput from 'material-ui-chip-input';
 import MagnifyingGlass from '../../images/magnifying-glass.svg';
 import DevelopmentalStageFilter from '../Common/DevelopmentalStageFilter';
 import CLOSE from '../../images/icon-close.svg';
+import REMOVE from '../../images/remove.svg';
 
 const Search = (props) => {
   const {
@@ -16,26 +18,54 @@ const Search = (props) => {
     setTimeout(() => setSearching(false), 1000);
   };
 
-  const search = (value) => {
-    setSearchTerm(value);
-    if (searchTerm.length >= 3) {
-      searchLoader();
+  function addSearchTerm(value) {
+    if (searchTerm.indexOf(value) > -1) {
+      return false;
     }
+    setSearchTerm([...searchTerm, value]);
+    searchLoader();
+    return true;
+  }
+
+  const removeSearchTerm = (value) => {
+    const filteredList = searchTerm.filter((item) => item !== value);
+    setSearchTerm(filteredList);
   };
 
   return (
     <Box className="wrap">
-      <TextField
-        fullWidth
-        placeholder="Filter by Name"
-        variant="outlined"
-        InputProps={{
-          startAdornment: <InputAdornment position="start"><img src={MagnifyingGlass} alt="MagnifyingGlass" /></InputAdornment>,
-          endAdornment: <InputAdornment position="end"><IconButton onClick={() => search('')}><img src={CLOSE} alt="CLOSE" /></IconButton></InputAdornment>,
-        }}
-        onChange={(e) => search(e.target.value)}
-        value={searchTerm}
-      />
+      <Box position="relative" className="search-bar">
+        <img src={MagnifyingGlass} alt="MagnifyingGlass" />
+        <ChipInput
+          variant="outlined"
+          onBeforeAdd={addSearchTerm}
+          defaultValue={[]}
+          value={searchTerm}
+          placeholder="Filter by Name"
+          fullWidth
+          chipRenderer={(
+            {
+              value,
+              className,
+            },
+            key,
+          ) => (
+            <Chip
+              key={key}
+              className={className}
+              onDelete={() => removeSearchTerm(value)}
+              label={value}
+              deleteIcon={<IconButton><img src={REMOVE} alt="REMOVE" /></IconButton>}
+            />
+          )}
+        />
+
+        {searchTerm.length ? (
+          <IconButton onClick={() => setSearchTerm([])}>
+            <img src={CLOSE} alt="CLOSE" />
+          </IconButton>
+        ) : null}
+      </Box>
 
       <Box display="flex" alignItems="center" justifyContent="space-between">
         <Typography variant="caption">Filter by Developmental Stage</Typography>
