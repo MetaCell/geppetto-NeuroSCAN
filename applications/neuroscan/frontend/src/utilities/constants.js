ed3449c1d56668eb1ccf54b6a74ab8e1e33ec617
Zoran Sinnema <zsinnema@gmail.com>
2021-09-07 19:21:16 +0200

    Feature/impl ch (#154)
    * move to cloudharness
    * chore: update deployment files

---
+++ applications/neuroscan/frontend/src/utilities/constants.js
@@ -0,0 +1,14 @@
+export const VIEWS = {
+  promoterDB: { title: 'Promoter DB', linkTo: 'NeuroSCAN', linkToRoute: '/' },
+  neuroScan: { title: 'NeuroSCAN', linkTo: 'Promoter DB', linkToRoute: '/promoter' },
+};
+
+export const VIEWERS = Object.freeze({
+  InstanceViewer: 'InstanceViewer',
+});
+
+export const backendURL = process.env.REACT_APP_BACKEND_URL || '/';
+console.log(`backend url: ${backendURL}`);
+
+export const ABOUT_CONTENT = `is an initiative from the Yale University for Neurosciences, in partnership
+with MetaCell.`;
