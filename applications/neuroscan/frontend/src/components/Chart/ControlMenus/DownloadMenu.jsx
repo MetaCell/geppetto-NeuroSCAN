872794ce42a979e32a3c0414ebf128da9c747e7e
vidhya-metacell <vidhya@metacell.us>
2021-09-21 15:42:49 +0300

    #156 Download Menu component integration

--- applications/neuroscan/frontend/src/components/Chart/ControlMenus/DownloadMenu.jsx
+++ applications/neuroscan/frontend/src/components/Chart/ControlMenus/DownloadMenu.jsx
@@ -12,8 +12,22 @@ import OBJ from '../../../images/obj.svg';
 import SCREENSHOT from '../../../images/screenshot.svg';
 import TICK from '../../../images/tick-circle.svg';
 
-const DownloadMenu = () => {
-  const [selectedValue, setSelectedValue] = useState('a');
+const DownloadMenu = ({ downloadFiles }) => {
+  const controls = [
+    {
+      value: 'object',
+      image: OBJ,
+      unselectedImage: OBJ,
+      displayText: 'OBJs files',
+    },
+    {
+      value: 'screenshot',
+      image: SCREENSHOT,
+      unselectedImage: SCREENSHOT,
+      displayText: 'Screenshot',
+    },
+  ];
+  const [selectedValue, setSelectedValue] = useState(controls[0]?.value);
 
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
@@ -23,38 +37,30 @@ const DownloadMenu = () => {
     <Box className="download">
       <Box className="download-body">
         <List>
-          <ListItem>
-            <Radio
-              checked={selectedValue === 'a'}
-              onChange={handleChange}
-              value="a"
-              name="radio-buttons"
-              inputProps={{ 'aria-label': 'A' }}
-            />
-            <ListItemText>
-              {selectedValue === 'a' && <img className="tick" src={TICK} alt="tick" />}
-              {selectedValue === 'a' ? <img src={OBJ} alt="OBJ" /> : <img src={OBJ} alt="OBJ" />}
-              <Typography>OBJs files</Typography>
-            </ListItemText>
-          </ListItem>
-          <ListItem>
-            <Radio
-              checked={selectedValue === 'b'}
-              onChange={handleChange}
-              value="b"
-              name="radio-buttons"
-              inputProps={{ 'aria-label': 'B' }}
-            />
-            <ListItemText>
-              {selectedValue === 'b' && <img className="tick" src={TICK} alt="tick" />}
-              {selectedValue === 'b' ? <img src={SCREENSHOT} alt="Screenshot" /> : <img src={SCREENSHOT} alt="Screenshot" />}
-              <Typography>Screenshot</Typography>
-            </ListItemText>
-          </ListItem>
+          {
+            controls.map((control, index) => (
+              <ListItem key={`radio_list${index}`}>
+                <Radio
+                  checked={selectedValue === control?.value}
+                  onChange={handleChange}
+                  value={control?.value}
+                  name="radio-buttons"
+                  inputProps={{ 'aria-label': control?.value }}
+                />
+                <ListItemText>
+                  {selectedValue === control?.value && <img className="tick" src={TICK} alt="tick" />}
+                  {selectedValue === control?.value
+                    ? <img src={control?.image} alt={control?.value} />
+                    : <img src={control?.unselectedImage} alt={control?.value} />}
+                  <Typography>{control?.displayText}</Typography>
+                </ListItemText>
+              </ListItem>
+            ))
+          }
         </List>
       </Box>
       <Box className="download-footer">
-        <Button disableElevation color="primary" variant="contained">
+        <Button disableElevation color="primary" variant="contained" onClick={() => downloadFiles(selectedValue)}>
           Download
         </Button>
       </Box>
--- applications/neuroscan/frontend/src/components/Chart/MenuControl.jsx
+++ applications/neuroscan/frontend/src/components/Chart/MenuControl.jsx
@@ -11,8 +11,13 @@ const MenuControl = ({
   anchorEl, handleClose, open, id, selection,
 }) => {
   const [content, setContent] = useState(null);
-  const [developmentalStage, setDevelopmentalStage] = React.useState(0);
+  const [developmentalStage, setDevelopmentalStage] = useState(0);
   const layersList = ['Worm Body', 'Pharynx', 'NerveRing'];
+  const downloadFiles = (option) => {
+    console.log(`selected option: ${option}`);
+    handleClose();
+  };
+
   useEffect(() => {
     switch (selection) {
       case VIEWER_MENU.devStage: setContent(
@@ -24,7 +29,7 @@ const MenuControl = ({
         break;
       case VIEWER_MENU.layers: setContent(<LayersMenu layers={layersList} />);
         break;
-      case VIEWER_MENU.download: setContent(<DownloadMenu />);
+      case VIEWER_MENU.download: setContent(<DownloadMenu downloadFiles={downloadFiles} />);
         break;
       default:
         setContent(null);
