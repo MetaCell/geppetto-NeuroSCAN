53b35d78f4355e5d857ed04c2a4c104956d6b149
Zoran Sinnema <zsinnema@gmail.com>
2021-12-04 13:05:15 +0100

    #217 fix: fixes bug opening colorpicker when no cluster is selected

--- applications/neuroscan/frontend/src/components/Chart/ControlMenus/ColorPickerMenu.jsx
+++ applications/neuroscan/frontend/src/components/Chart/ControlMenus/ColorPickerMenu.jsx
@@ -37,7 +37,7 @@ const ColorPickerMenu = ({
   });
 
   const [expanded, setExpanded] = useState([
-    `iteration-${clusters.find((i) => i.selected).i}`,
+    `iteration-${clusters.find((i) => i.selected)?.i || {}}`,
   ]);
 
   const handleChangeComplete = (data) => {
