c51af7e7b3f5ed503679640a94b6d491da2b4577
Zoran Sinnema <zsinnema@gmail.com>
2021-12-09 14:04:59 +0100

    #ingestion_loaders chore: added promoter loader

--- ingestion/ingestion/loaders/utils.py
+++ ingestion/ingestion/loaders/utils.py
@@ -14,3 +14,8 @@ def getContact(cur: Cursor, uid: str, timepoint: int) -> []:
 def getSynapse(cur: Cursor, uid: str, timepoint: int) -> []:
     q = cur.execute("select * from synapses where uid=(?) and timepoint=(?)", [uid, timepoint])
     return q.fetchone()
+
+
+def getPromoter(cur: Cursor, uid: str) -> []:
+    q = cur.execute("select * from promoters where uid=(?)", [uid])
+    return q.fetchone()
