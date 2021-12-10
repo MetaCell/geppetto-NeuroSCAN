f4d8476cb6d0af8e75e1a8632b04002f75a7a571
Dario Del Piano <dario@metacell.us>
2021-12-10 18:29:48 +0000

    #geppetto feature/257 setting depthWrite to true

--- applications/neuroscan/frontend/src/utilities/functions.js
+++ applications/neuroscan/frontend/src/utilities/functions.js
@@ -65,6 +65,7 @@ export const addToWidget = (
         reset: false,
         autorotate: false,
         wireframe: false,
+        depthWrite: false,
       },
       captureOptions: {
         captureControls: {
