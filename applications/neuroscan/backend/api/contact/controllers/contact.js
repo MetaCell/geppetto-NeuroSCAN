d64b73c485c2f22a29c445814d74b972fc5951c8
afonso pinto <afonsobspinto@gmail.com>
2023-10-27 12:04:06 +0100

    fix: Allow default strapi search when no terms are passed on contacts

--- applications/neuroscan/backend/api/contact/controllers/contact.js
+++ applications/neuroscan/backend/api/contact/controllers/contact.js
@@ -9,18 +9,24 @@ const {sanitizeEntity} = require('strapi-utils');
 
 module.exports = {
   async find(ctx) {
-    const terms = ctx.query.terms ? ctx.query.terms.split(',').map(t => t.toLowerCase()) : [];
-    const start = parseInt(ctx.query._start || "0");
-    const limit = parseInt(ctx.query._limit || "30");
-    const timepoint = ctx.query.timepoint
+    let entities;
 
-    let entities = await strapi.services.contact.customSearch(timepoint, terms, start, limit);
-    entities = entities.map(entity => {
-      return {
-        ...entity,
-        name: entity.name ? entity.name : `${entity.neuronA_uid} contact from ${entity.neuronB_uid}`
-      };
-    });
+    if (ctx.query.terms != null) {
+      const terms = ctx.query.terms.split(',').map(t => t.toLowerCase());
+      const start = parseInt(ctx.query._start || "0");
+      const limit = parseInt(ctx.query._limit || "30");
+      const timepoint = ctx.query.timepoint
+
+      entities = await strapi.services.contact.customSearch(timepoint, terms, start, limit);
+      entities = entities.map(entity => {
+        return {
+          ...entity,
+          name: entity.name ? entity.name : `${entity.neuronA_uid} contact from ${entity.neuronB_uid}`
+        };
+      });
+    } else{
+      entities = await strapi.services.contact.find(ctx.query);
+    }
     return entities.map(entity => {
       return sanitizeEntity(entity, {
         model: strapi.models.contact,
